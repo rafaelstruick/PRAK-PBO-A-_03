316dbb9f19e378662bb54bc19bcda6405867e811
rafaelstruick <deswitaprsdeibago@gmail.com>
2024-09-26 16:47:49 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,20 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        BangunDatar bangunDatar = new BangunDatar();
-        Persegi persegi = new Persegi(3);
-        Segitiga segitiga = new Segitiga(4, 4);
-        Lingkaran lingkaran = new Lingkaran(7);
-        
-        // memanggil method luas dan keliling
-        bangunDatar.luas();
-        bangunDatar.keliling();
-        System.out.print("\n");
-        System.out.println("Luas persegi: " + persegi.luas());
-        System.out.println("keliling persegi: " + persegi.keliling());
-        System.out.print("\n");
-        System.out.println("Luas segitiga: " + segitiga.luas());
-        System.out.print("\n");
-        System.out.println("Luas lingkaran: " + lingkaran.luas());
-        System.out.println("keliling lingkaran: " + lingkaran.keliling());
+
+    public static void main(String[] args) {
+        Binatang kambing = new Binatang("Kambing");
+        Kucing mudreg = new Kucing("mudreg");
+        Cemeng mocha = new Cemeng("mocha");
+
+        kambing.bersuara();
+        mudreg.bersuara();
+        mocha.bersuara();
     }
 }
